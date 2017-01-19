7988ffd2a77915223ff68853f98b7a187db3dfc6
alanna-wing <alanna.wing@gmail.com>
2017-01-19 15:33:37 +0000

    added all function up to average_bank_account_value

--- start_point/bank_account_functions.rb
+++ start_point/bank_account_functions.rb
@@ -13,3 +13,25 @@ def bank_account_owner_names( accounts )
   end
   return bank_account_owner_names
 end
+
+def total_cash_in_bank( accounts )
+  total_cash = 0
+  for account in accounts
+    total_cash += account["amount"]
+  end
+  return total_cash
+end
+
+def total_business_cash_in_bank( accounts )
+  total_cash_business = 0
+  for account in accounts
+    if account["type"] == "business"
+      total_cash_business += account["amount"]
+    end
+  end
+  return total_cash_business
+end
+
+def average_bank_account_value( accounts )
+
+end
