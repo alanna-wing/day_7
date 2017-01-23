1165c0618457ad9e5a84ccd142669571068e9ec5
alanna-wing <alanna.wing@gmail.com>
2017-01-23 21:42:11 +0000

    completed homework, all tests pass

--- start_point/bank_account_functions.rb
+++ start_point/bank_account_functions.rb
@@ -33,5 +33,61 @@ def total_business_cash_in_bank( accounts )
 end
 
 def average_bank_account_value( accounts )
+  average_account_value = 0
+  for account in accounts
+    average_account_value += account["amount"]
+  end
+  return average_account_value / accounts.count()
+end
 
+#created variable as counter and add 1 for each high risk account
+# def find_accounts_by_risk( accounts, risk_level )
+#   high_risk_accounts = 0
+#   for account in accounts
+#     if account["stats"]["risk_level"] == "high"
+#       high_risk_accounts += 1
+#     end
+#   end
+#   return high_risk_accounts
+# end
+
+#pushed high risk accounts into empty array and called count method on variable
+def find_accounts_by_risk( accounts, risk_level )
+  high_risk_accounts = []
+  for account in accounts
+    if account["stats"]["risk_level"] == "high"
+      high_risk_accounts.push(account)
+    end
+  end
+  return high_risk_accounts.length()
+end
+
+def name_of_largest_value_account_holder( accounts, amount )
+  largest_value_account_holder = accounts[0]
+  for account in accounts
+    if account["amount"] > largest_value_account_holder["amount"]
+      largest_value_account_holder = account
+    end
+  end
+  return largest_value_account_holder["holder_name"]
+end
+
+def name_of_largest_personal_account_holder( accounts, type )
+  largest_personal_account_holder = accounts[0]
+  for account in accounts
+    if account["amount"] > largest_personal_account_holder["amount"] && account["type"] == "personal"
+      largest_personal_account_holder = account
+    end
+  end
+  return largest_personal_account_holder["holder_name"]
+end
+
+def name_of_largest_business_account_holder( accounts, type )
+  largest_business_account_holder = accounts[0]
+  for account in accounts
+    if account["amount"] > largest_business_account_holder["amount"] && account["type"] == "business"
+      largest_business_account_holder = account
+    end
+  end
+  return largest_business_account_holder["holder_name"]
 end
--- start_point/specs/bank_accounts_spec.rb
+++ start_point/specs/bank_accounts_spec.rb
@@ -25,39 +25,39 @@ class BankTest < MiniTest::Test
   end
 
   def test_total_cash_in_bank
-    skip
     total_cash = total_cash_in_bank(@accounts)
     assert_equal(7311, total_cash)
   end
 
   def test_total_cash_in_business_accounts
-    skip
     total_cash_business = total_business_cash_in_bank(@accounts)
     assert_equal(7055,total_cash_business)
   end
 
   def test_average_bank_account_value
-    skip
     average_value = average_bank_account_value(@accounts)
     assert_equal(913,average_value)
   end
 
   def test_find_accounts_by_risk
-    skip
     accounts_by_risk = find_accounts_by_risk(@accounts, "high")
-    assert_equal(3, accounts_by_risk.length())
+    assert_equal(3, accounts_by_risk)
   end
 
+#homework
   def test_name_of_largest_value_account_holder
-    skip
+    largest_value_account_holder = name_of_largest_value_account_holder(@accounts, 4500)
+    assert_equal("Sandy", largest_value_account_holder)
   end
 
   def test_name_of_largest_personal_account_holder
-    skip
+    largest_personal_account_holder = name_of_largest_personal_account_holder(@accounts, "personal")
+    assert_equal("Tony", largest_personal_account_holder)
   end
 
   def test_name_of_largest_business_account_holder
-    skip
+    largest_business_account_holder = name_of_largest_business_account_holder(@accounts, "business")
+    assert_equal("Sandy", largest_business_account_holder)
   end
 
 end
